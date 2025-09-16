25ffdf78ade7a56e56f5eb5d5ebbcc1717436b4f
Santanu Deb <69859172+santanu603@users.noreply.github.com>
2025-09-16 16:23:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js + Jenkins on CentOS 9 🚀");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running at http://0.0.0.0:${PORT}`);
 });
