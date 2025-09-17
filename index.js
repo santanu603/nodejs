94b2f2632e3da539a74de3f426502fb8562c98b2
Santanu Deb <69859172+santanu603@users.noreply.github.com>
2025-09-17 10:31:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Node.js + Jenkins on CentOS 9 🚀");
 });
 
-app.listen(PORT, "192.168.0.104", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running at http://0.0.0.0:${PORT}`);
 });
